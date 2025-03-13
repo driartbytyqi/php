9bbd17204b48588944619c8f7bc8536b5584f83c
driartbytyqi <driartbytyqii@gmail.com>
2025-03-13 08:10:40 -0700

    ndryshim

---
+++ lesson2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = 'digitalschool';
+    echo "i love $school" . "<br>";
+
+    $x = 120;
+    $y = 50;
+
+    echo $x + $y . "<br>";
+    echo $x - $y . "<br>";
+    echo $x / $y . "<br>";
+    echo $x * $y . "<br>";
+    echo $x % $y . "<br>";
+    $a = 'digital';
+    $b ='school';
+
+    $c = $a . $b;
+    echo = "$c\n";
+
+    $first-string = 'filan fisteku';
+    echo ($first-string);
+    $second-string = 'filan fisteku';
+    echo str-world-count($second-string);
+    $third-string='programmingisnot cool';
+    echo str-word-replace('not','very',$third-string);
+    $fourth-string='real madrit';
+    echo strev($fourth-string)
+    ?>
+    
+
+
+</body>
+</html>
