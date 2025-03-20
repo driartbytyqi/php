8bf6e175779fae71fcc56c4feaebfc2396546881
driartbytyqi <driartbytyqii@gmail.com>
2025-03-20 07:59:07 -0700

    lesson3

--- lesson3/index.php
+++ lesson3/index.php
@@ -49,9 +49,59 @@
         default:
         echo "invalid age input.";
     }
-    $day = "e enjte"
+  
+    $day = "e enjte";
     switch($day){
-        case($day == "e enjte");
+        case "e hene":
+            echo "sot eshte dita ehene";
+            break;
+        case "e marte":
+            echo "sot eshte dita e marte ";
+            break;
+         case "e merkur":
+            echo "sot eshte dita e merkur";
+            break;
+        case "e enjte":
+            echo "sot eshte dita e enjte";
+            break;
+        case "e premte":
+            echo "sot eshte dita e premte";
+             break;
+        case "e shtun":
+            echo "sot eshte dita e shtun";
+            break;
+        case "e dielle":
+            echo "sot eshte dita e dielle";
+            break;
+    }
+
+    $whilevar = 1;
+    while($whilevar <= 5){
+        echo"<br> numri is $whilevar <br>";
+        $whilevar++;
+    }
+    $dowhile =1;
+    do{
+        echo"<br> numri is $dowhile <br>";
+        $dowhile++;.
+    }while($whilevar <= 5);
+    for($forvar = 0;$forvar<= 10; $forvar++){
+        echo"<br> numri is $forvar <br>";
+    }
+    
+
+
+    ?>
+    <?php
+    $cars = array("volvo","bmw","tesla","audi");
+    foreach($cars as $value){
+        echo "$value <br>";
+    }
+    ?>
+    <?php
+    $age3 = array("John" => "30", "Mary" => "25", "David" =>"10");
+    foreach($age3 as $x1 => $val){
+        echo "name: $x1 , age: $val <br>";
     }
     ?>
 </body>
