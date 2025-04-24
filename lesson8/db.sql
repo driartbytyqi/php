ed346690a13dee987e11aacb2977aa426877c047
driartbytyqi <driartbytyqii@gmail.com>
2025-04-24 16:16:28 +0200

    lesson8

---
+++ lesson8/db.sql
@@ -0,0 +1,4 @@
+CREATE TABLE categoris(
+    id Integer primary key,
+    name varchar (225) not null,
+)
---
+++ lesson8/index.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'local host';
+$db = 'db';
+$user = 'root';
+$pass = '';
+
+try{
+    $pdo = new PDO("mysql:host=$host; dbname=$db",$user,$pass);
+    $sql = "CREATE TABLE user(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL)";
+
+    $pdo -> exec($sql);
+    echo "table created successfuly";
+}
+catch (Exeption $e){
+    echo "error creating table:". $e ->getMessage();
+}
+
+?>
