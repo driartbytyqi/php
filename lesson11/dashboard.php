ecc131ed2d22cc2ba0fc9dd62ee199c1dc71b68a
driartbytyqi <driartbytyqii@gmail.com>
2025-05-15 16:06:39 +0200

    lesson12

--- lesson11/dashboard.php
+++ lesson11/dashboard.php
@@ -15,6 +15,14 @@
         </style>
 </head>
 <body>
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+    </a>
+  </div>
+</nav>
+
     <?php
     include_once("config.php");
     $sql = "SELECT = * FROM users";
